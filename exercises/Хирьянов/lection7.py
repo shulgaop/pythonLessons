afb9a9bcedcf46adc32e54738b16e8bc7f94d4ba
Aleh Shulha <69508780+shulgaop@users.noreply.github.com>
2020-11-05 17:14:28 +0300

    05.11.2020 15:36

--- exercises/Хирьянов/lection7.py
+++ exercises/Хирьянов/lection7.py
@@ -7,16 +7,20 @@ def canvas_click_handler(event):
     print('HI x = ', event.x, 'y=', event.y)
 
 def tick():
-    print("MOVE")
-    global x,y
-    x += 1
-    y += 1
-    canvas.move(ball_id, +1, +1)
+
+    global x, y, dx, dy
+    x += dx
+    y += dy
+    if x + R > WIDTH or x - R <= 0:
+        dx = -dx
+    if y + R > WIDTH or x - R <= 0:
+        dy = -dy
+    canvas.move(ball_id, dx, dy)
     root.after(50, tick)
 
 def main():
     global root, canvas
-    global ball_id, x, y, z # TODO: сделать рефакторинг
+    global ball_id, x, y, dx, dy, R # TODO: сделать рефакторинг
 
     root = tk.Tk()
     root.geometry(str(WIDTH) + "x" + str(HEIGHT))
@@ -26,6 +30,7 @@ def main():
     R = randint(20, 50)
     x = randint(R, WIDTH - R)
     y = randint(R, HEIGHT - R)
+    dx, dy = (+1, +1)
     ball_id = canvas.create_oval(x - R, y - R, x + R, y + R, fill = 'green')
     tick()
     root.mainloop()
