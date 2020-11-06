daf7d6319b4f1af2ccc109ddafe21e69b8398a03
Aleh Shulha <69508780+shulgaop@users.noreply.github.com>
2020-11-06 15:44:15 +0300

    работает

--- exercises/Хирьянов/lection8_bals refactoring.py
+++ exercises/Хирьянов/lection8_bals refactoring.py
@@ -14,21 +14,21 @@ class Ball:
                                      self.x + self.R,
                                      self.y + self.R,
                                      fill='green')
-        tick()
-        pass
+
 
     def move(self):
-        global x, y, dx, dy
         self.x += self.dx
         self.y += self.dy
-        if self.x + self.R > WIDTH or self.x - self.R <= 0:
+        if self.x + self.R > WIDTH or self.x - self.R\
+                <= 0:
             self.dx = -self.dx
-        if self.y + self.R > HEIGHT or self.x - self.R <= 0:
+        if self.y + self.R > HEIGHT or self.x - self.R\
+                <= 0:
             self.dy = -self.dy
 
     def show(self):
         canvas.move(self.ball_id, self.dx, self.dy)
-        root.after(50, tick)
+
 
 
 
@@ -36,18 +36,19 @@ def canvas_click_handler(event):
     print('HI x = ', event.x, 'y=', event.y)
 
 def tick():
+    ball.move()
+    ball.show()
     root.after(50, tick)
 
-def main():
-    global root, canvas
-    global ball_id, x, y, dx, dy, R # TODO: сделать рефакторинг
 
+def main():
+    global root, canvas, ball
     root = tk.Tk()
     root.geometry(str(WIDTH) + "x" + str(HEIGHT))
     canvas = tk.Canvas(root)
     canvas.pack(anchor="nw", fill=tk.BOTH)
     canvas.bind('<Button-1>', canvas_click_handler)
-
+    ball = Ball()
     tick()
     root.mainloop()
 if __name__ == "__main__":
